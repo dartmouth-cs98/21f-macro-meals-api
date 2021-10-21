0ad5be293e913d883a0f55ce06c95e9d827c1f5a
Quinn Spraut <quinn.a.spraut.22@dartmouth.edu>
2021-10-21 17:16:10 -0400

    adding access control rights

--- src/router.js
+++ src/router.js
@@ -3,6 +3,12 @@ import * as Food from './controllers/food_controller';
 
 const router = Router();
 
+router.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 router.get('/', (req, res) => {
   res.json({ message: 'Welcome to Macro API!' });
 });
