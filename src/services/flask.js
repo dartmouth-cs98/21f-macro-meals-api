855dc9b0e7b73a01c89c073630720ce7ef739792
Quinn Spraut <quinn.a.spraut.22@dartmouth.edu>
2021-11-12 17:49:32 -0500

    adding error handling

--- src/services/flask.js
+++ src/services/flask.js
@@ -3,13 +3,13 @@ import axios from 'axios';
 const classifyFlask = (req, res) => {
   axios
     .post('https://macroclassifier.herokuapp.com/', {
-      url: req.url
+      url: req.url,
     })
     .then((response) => {
       res.send(JSON.stringify(response.data));
     })
     .catch((error) => {
-      res.send(error);
+      res.send(error.response.data);
     });
 };
 
