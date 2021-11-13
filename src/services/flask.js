0efb5a3a56a668c134f252f2926517f80eabe1cb
Quinn Spraut <quinn.a.spraut.22@dartmouth.edu>
2021-11-12 19:36:29 -0500

    reverting

--- src/services/flask.js
+++ src/services/flask.js
@@ -4,8 +4,6 @@ const classifyFlask = (req, res) => {
   axios
     .post('https://macroclassifier.herokuapp.com/', {
       url: req.body.url,
-    }, {
-      'content-type': 'text/plain',
     })
     .then((response) => {
       res.send(JSON.stringify(response.data));
